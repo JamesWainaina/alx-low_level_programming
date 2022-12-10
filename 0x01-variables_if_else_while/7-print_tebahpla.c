fc3e15677e269d5af818aa8f42ca0106be835835
James Gatheru <jamesgatheru9905@gmail.com>
2022-12-09 17:39:33 -0800

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * label: prints letters in reverse
+ *
+ * Return:0
+ */
+int main(void)
+{
+	char t = 'z';
+
+	while (t >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+
+	putchar('\n');
+	return (0);
+}
