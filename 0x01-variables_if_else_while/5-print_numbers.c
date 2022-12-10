45dd38e21842c78e693e725364ca28770166bb83
James Gatheru <jamesgatheru9905@gmail.com>
2022-12-09 17:15:02 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ * Label: Printing numbers from the base of ten
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char t = '0';
+
+	while (c <= '9')
+	[
+		putchar(t);
+		t++;
+	}
+
+	putchar('\n');
+	return (0);
+}
