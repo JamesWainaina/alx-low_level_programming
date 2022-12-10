86f26832b32a6ca95e5b344688f3cd7bc045e3cf
James Gatheru <jamesgatheru9905@gmail.com>
2022-12-09 16:52:38 -0800

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,25 @@
+# include <stdio.h>
+/**
+ * main - main block
+ * label - print alphabet in lower case and upper case
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char t  = 'a';
+
+	while (t <= 'z')
+	{
+		putchar(t);
+		t++;
+	}
+
+	t = 'A';
+
+	while (t <= 'z')
+	{
+		putchar(t);
+		t++;
+	}
+}
