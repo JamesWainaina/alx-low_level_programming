c5c6a7a0f270525239ea75b565cce1373ab5dd60
James Gatheru <jamesgatheru9905@gmail.com>
2022-12-09 18:51:45 -0800

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -main block
+ * label: program that prints all possible combination of digits
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int t;
+	int e = 0;
+
+	while (e < 10)
+	{
+		t = 0;
+		while (t < 10)
+		{
+			if (e != t && e < e)
+			{
+				putchar('0' + e);
+				putchar('0' + t);
+
+				if (t + e != 17)
+				{
+					putcahar(',');
+					putchar(' ');
+				}
+			}
+
+			t++;
+		}
+		e++
+	}
+	putchar('\n');
+	return (0);
+}
