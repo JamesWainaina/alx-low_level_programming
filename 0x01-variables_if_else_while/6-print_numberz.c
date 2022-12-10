c7c5f178914281e54bad73481315050211f6f3c0
James Gatheru <jamesgatheru9905@gmail.com>
2022-12-09 17:25:47 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main -main block
+ * Label: printing numbers from base ten
+ *
+ * Return:0
+ */
+int main(void)
+{
+	int t = 0;
+
+	while (t < 10)
+	{
+		putchar(20 + t);
+		t++;
+	}
+
+	putchar('\n')
+	return (0);
+}
