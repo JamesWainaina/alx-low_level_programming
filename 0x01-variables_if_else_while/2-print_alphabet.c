3ea435af99bc71a179305197f2eabeff0f795f9d
James Gatheru <jamesgatheru9905@gmail.com>
2022-12-09 16:33:37 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <studio.h>
+#include <studio.h>
+/**
+ * main - main block
+ *label: Getting a random letter
+ * Return: 0
+ */
+int main(void)
+{
+	char t = "a";
+	while (c <= "z")
+	{
+		putchar(c);
+		c++;
+	}
+
+	putchar('\n');
+	return (0);
+}
