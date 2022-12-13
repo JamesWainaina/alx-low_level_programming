e6b61e8be8b207b06741e4b54564b1b3c764a0fb
James Gatheru <jamesgatheru9905@gmail.com>
2022-12-13 09:47:26 -0800

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,9 +5,8 @@
  * Return: 0
  */
 
-int main(void)
+int main(void);
 {
-
 _putchar('_');
 _putchar('p');
 _putchar('u');
@@ -18,6 +17,5 @@ _putchar('a');
 _putchar('r');
 _putchar('\n');
 return (0);
-
 }
 
