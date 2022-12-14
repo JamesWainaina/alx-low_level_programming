d9bf10ed3c57779567efd68e5ec359161e068c12
James Gatheru <jamesgatheru9905@gmail.com>
2022-12-14 06:11:25 -0800

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -14,12 +14,12 @@ void times_table(void)
 		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
-			_putcahar(' ');
+			_putchar(' ');
 			prod = num * mult;
 			if (prod <= 9)
 				_putchar(' ');
 			else
-				_putcahr((prod / 10) + '0');
+				_putchar((prod / 10) + '0');
 
 			_putchar((prod % 10) + '0');
 		}
