f555a4dadd8afd345db8cf1fff5782488413eb37
James Gatheru <jamesgatheru9905@gmail.com>
2022-12-14 22:46:11 -0800

    task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -2,13 +2,13 @@
 
 /**
  * main - Prints the sum of even-valued Fibonacci sequence
- * 	terms not exceeding 4000000.
+ * terms not exceeding 4000000.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	unsigned long fib1 = 0, fib2 =1, fibsum;
+	unsigned long fib1 = 0, fib2 = 1, fibsum;
 	float tot_sum;
 
 	while (1)
