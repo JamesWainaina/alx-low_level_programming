c9fba22c2f2bb489c847f84e200692d2ce89e76f
James Gatheru <jamesgatheru9905@gmail.com>
2022-12-15 04:15:51 -0800

    task 6

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,7 +14,7 @@ void print_diagonal(int n)
 		{
 			for (space = 0; space < len; space++)
 				_putchar(' ');
-			_putcahr('\\');
+			_putchar('\\');
 			if (len == n - 1)
 				continue;
 			_putchar('\n');
