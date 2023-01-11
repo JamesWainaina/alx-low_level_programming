6a1653889c4323e682f6cf0a5dc0b00b4039d2dd
James Gatheru <jamesgatheru9905@gmail.com>
2023-01-11 05:11:31 -0800

    task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,20 @@
+/*
+ * file: 0-malloc_checked.c
+ * Auth: Brennan D baraban
+ */
+#include "main.h"
+#include  <stdlib.h>
+/**
+ * malloc_checked - Allocates memeory using malloc.
+ * @b: The number of bytes to be allocated.
+ *
+ * Return: A pointer to the allocatewd memory.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *mem = malloc(b);
+
+	if (mem == NULL)
+		exit(98);
+	return (mem);
+}
