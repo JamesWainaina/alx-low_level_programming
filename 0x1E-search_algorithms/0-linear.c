76c2d996041b37906193e1fd9014ad8f7f6fb947
JamesWainaina <jamesgatheru9905@gmail.com>
2023-12-15 11:39:14 +0300

    worst case linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,7 +1,4 @@
 #include "search_algos.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 
 /**
  * linear_search - searches for a value in an array of
@@ -13,18 +10,19 @@
  * Return: the index of the found value,
  * or -1 if not found
  */
+
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
 	if (array == NULL)
-			return (-1);
-	
+		return (-1);
+
 	for (; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-				return (i);
+			return (i);
 	}
 
 	return (-1);
